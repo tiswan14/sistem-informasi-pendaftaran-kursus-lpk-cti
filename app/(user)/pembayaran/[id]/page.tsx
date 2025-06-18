6a44e4d6c2ec418f10adf13884813164f20f0c1d
tiswan14 <tiswan30@gmail.com>
2025-06-18 22:50:16 +0700

    fix: perbaiki tipe params pada dynamic route [id] agar build sukses di Railway

--- app/(user)/pembayaran/[id]/page.tsx
+++ app/(user)/pembayaran/[id]/page.tsx
@@ -1,18 +1,21 @@
-import { Metadata } from "next";
+// app/(user)/pembayaran/[id]/page.tsx
+
 import PembayaranDetailComponent from "@/components/user/pembayaran/PembayaranDetailComponent";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
     title: "Pembayaran Detail - LPK CTI Ciamis",
 };
 
 type Props = {
-    params: { id: string };
+    params: {
+        id: string;
+    };
 };
 
 export default function PembayaranDetailPage({ params }: Props) {
     return (
         <div className="py-32 px-16">
-            <h1>Semoga</h1>
             <PembayaranDetailComponent id={params.id} />
         </div>
     );
