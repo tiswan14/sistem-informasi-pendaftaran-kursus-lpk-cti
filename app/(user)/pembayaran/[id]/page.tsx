3e7be1924e22e868ffe4f117c3e2746f4fddd912
tiswan14 <tiswan30@gmail.com>
2025-06-18 22:40:36 +0700

    fix: hapus route kursusId error dan perbaiki struktur dynamic route [id]

--- app/(user)/pembayaran/[id]/page.tsx
+++ app/(user)/pembayaran/[id]/page.tsx
@@ -1,18 +1,18 @@
-import PembayaranDetailComponent from "@/components/user/pembayaran/PembayaranDetailComponent"
-import { Metadata } from "next"
+import { Metadata } from "next";
+import PembayaranDetailComponent from "@/components/user/pembayaran/PembayaranDetailComponent";
 
 export const metadata: Metadata = {
     title: "Pembayaran Detail - LPK CTI Ciamis",
-}
+};
 
-const PembayaranDetailPage = ({ params: { id } }: { params: { id: string } }) => {
+type Props = {
+    params: { id: string };
+};
+
+export default function PembayaranDetailPage({ params }: Props) {
     return (
-        <>
-            <div className="py-32 px-16">
-                <PembayaranDetailComponent id={id} />
-            </div>
-        </>
-    )
+        <div className="py-32 px-16">
+            <PembayaranDetailComponent id={params.id} />
+        </div>
+    );
 }
-
-export default PembayaranDetailPage
