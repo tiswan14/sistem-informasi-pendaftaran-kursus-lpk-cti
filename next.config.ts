d3d6c1e7d50fa6b0149f8cd707e58a6f904789bf
tiswan14 <tiswan30@gmail.com>
2025-06-19 15:04:44 +0700

    fix: deployment error

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['lblrcuwfaaqduaps.public.blob.vercel-storage.com'],
   },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  } as any, // ✅ bypass type error hanya di properti ini
 };
 
 export default nextConfig;
