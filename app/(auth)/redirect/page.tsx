40f280d3e7c8e4f38ee13befa3dbab5650188895
tiswan14 <tiswan30@gmail.com>
2025-06-20 11:04:33 +0700

    fix: router from next navigation

--- app/(auth)/redirect/page.tsx
+++ app/(auth)/redirect/page.tsx
@@ -1,6 +1,6 @@
 import { useSession } from "next-auth/react";
 import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function RedirectByRole() {
     const { data: session, status } = useSession();
